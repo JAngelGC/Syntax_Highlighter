3f7450fb383b3434972175fb7d01b6018618e1e4
JAngelGC <a01552274@tec.mx>
2022-04-16 13:54:42 -0500

    Add connection between elixir and javascript through htmlFile.txt

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const insertHtmlCode = function () {
+  fetch("../syntax/htmlFile.txt")
+    .then((response) => response.text())
+    .then((data) => {
+      const newData = data.split(",").join("");
+      console.log(newData);
+
+      const containerCode = document.querySelector("#insert");
+
+      containerCode.insertAdjacentHTML("afterbegin", newData);
+    })
+    .catch((err) => console.log(err));
+};
+
+insertHtmlCode();
+
+//   Podemos agregar un event listener para que se refresque el contenido cada que se recibe un nuevo archivo
+//   Checar como hacerlo
--- syntax/lib/syntax.ex
+++ syntax/lib/syntax.ex
@@ -1,5 +1,4 @@
 defmodule Syntax do
-
   # area = pi * radio ** 2
   # :lexer.string('area = pi * radio ** 2')
   # Syntax.format(elem(:lexer.string('area = pi * radio ** 2'),1))
@@ -7,7 +6,8 @@ defmodule Syntax do
   # Syntax.format(elem(:lexer.string('  '),1))
 
   def syntax(text) do
-    Syntax.format(elem(:lexer.string(text),1))
+    htmlStr = format(elem(:lexer.string(text),1))
+    File.write("htmlFile.txt", htmlStr) #Ponemos el resultado en un archivo
   end
 
   def format(tokens) do
@@ -16,18 +16,26 @@ defmodule Syntax do
       tchars = HtmlEntities.encode(tchars)
 
       case token do
-        :space -> "<span class=\"space\">#{tchars}</span>"
-        :newline -> "<span class=\"newline\"><br></span>"
-        :identifier -> "<span class=\"identifier\">#{tchars}</span>"
-        :data_type -> "<span class=\"identifier\">#{tchars}</span>"
-        :data -> "<span class=\"identifier\">#{tchars}</span>"
-        :assign -> "<span class=\"identifier\">#{tchars}</span>"
-        :operator -> "<span class=\"identifier\">#{tchars}</span>"
-        :relation -> "<span class=\"identifier\">#{tchars}</span>"
-        :logical -> "<span class=\"identifier\">#{tchars}</span>"
+        :space -> "<span class='space'>#{tchars}</span>"
+        :newline -> "<span class='newline'><br></span>"
+        :identifier -> "<span class='identifier'>#{tchars}</span>"
+        :data_type -> "<span class='data-type'>#{tchars}</span>"
+        :data -> "<span class='data'>#{tchars}</span>"
+        :assign -> "<span class='assign'>#{tchars}</span>"
+        :operator -> "<span class='operator'>#{tchars}</span>"
+        :relation -> "<span class='relation'>#{tchars}</span>"
+        :logical -> "<span class='logical'>#{tchars}</span>"
         _ -> "<span>#{tchars}</span>"
       end
     end)
     |> Enum.join()
+
+    # File.write("htmlFile.txt", Syntax.format(elem(:lexer.string('area = pi * radio ** 2'),1)))
   end
+
+  # def write(tokens) do
+  #   htmlStr = format((tokens))
+  #   File.write("htmlFile.txt", htmlStr)
+  # end
+
 end
