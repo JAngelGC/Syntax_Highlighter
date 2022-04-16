175c093b217248aa8e99177a0387d83eeea5ffc7
Diaz <carlosd2mil@gmail.com>
2022-04-16 04:03:00 -0500

    Instructions, syntax function & cases
    Add test function so you only need to introduce the charlist

--- syntax/lib/syntax.ex
+++ syntax/lib/syntax.ex
@@ -1,7 +1,14 @@
 defmodule Syntax do
 
   # area = pi * radio ** 2
+  # :lexer.string('area = pi * radio ** 2')
+  # Syntax.format(elem(:lexer.string('area = pi * radio ** 2'),1))
   # tokens = [{:identifier, 'area'}, {:operator, '='}, {:identifier, 'pi'}, {:operator, '*'}, {:identifier, 'radio'}, {:operator, '**'}, {:int, '2'}]
+  # Syntax.format(elem(:lexer.string('  '),1))
+
+  def syntax(text) do
+    Syntax.format(elem(:lexer.string(text),1))
+  end
 
   def format(tokens) do
     Enum.map(tokens, fn {token, tchars} ->
@@ -9,6 +16,8 @@ defmodule Syntax do
       tchars = HtmlEntities.encode(tchars)
 
       case token do
+        :space -> "<span class=\"space\">#{tchars}</span>"
+        :newline -> "<span class=\"newline\"><br></span>"
         :identifier -> "<span class=\"identifier\">#{tchars}</span>"
         :data_type -> "<span class=\"identifier\">#{tchars}</span>"
         :data -> "<span class=\"identifier\">#{tchars}</span>"
@@ -19,5 +28,6 @@ defmodule Syntax do
         _ -> "<span>#{tchars}</span>"
       end
     end)
+    |> Enum.join()
   end
 end
